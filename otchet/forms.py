c5246c2ef210c7f33269c2cf86888cebcfdc0633
tich31337 <tich31337@ya.ru>
2015-08-10 04:48:29 +0500

    исправлены поля ввода времени (но не вводится время по умолчанию)

--- otchet/forms.py
+++ otchet/forms.py
@@ -2,7 +2,8 @@
 from .models import CustomUser, s_group, s_office, s_timetable, s_fault, s_drop_lift, s_commit
 from django import forms 
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from datetime import datetime 
+from datetime import datetime
+
 
 class AdminUserAddForm(UserCreationForm):
 
@@ -27,14 +28,14 @@ class AdminUserChangeForm(UserChangeForm):
         fields  = '__all__'
 
 class s_faultForm(forms.ModelForm):
-    fault_time = forms.DateTimeField(initial=datetime.now().strftime('%d.%m.%Y %H:%M'), label='Время')
+    fault_time = forms.DateTimeField(label='Время', widget=forms.TextInput(attrs={'type': 'datetime-local', 'value': '10. 08. 2015 10:00'}))
     class Meta:
         model = s_fault
         exclude = ('f_staff',)
 
 class s_drop_liftForm(forms.ModelForm):
-    stop_lift = forms.DateTimeField(initial=datetime.now().strftime('%d.%m.%Y %H:%M'), label = 'Время остановки')
-    start_lift = forms.DateTimeField(initial=datetime.now().strftime('%d.%m.%Y %H:%M'), label = 'Время запуска')
+    stop_lift = forms.DateTimeField(label = 'Время остановки', widget=forms.TextInput(attrs={'type': 'datetime-local'}))
+    start_lift = forms.DateTimeField(label = 'Время запуска', widget=forms.TextInput(attrs={'type': 'datetime-local'}))
     class Meta:
         model   = s_drop_lift
         fields  = '__all__'
