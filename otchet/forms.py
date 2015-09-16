936b2e45adbde2a3c5341a4d086f1d706c474205
tich <tich31337@ya.ru>
2015-09-17 00:53:05 +0500

    èñïðàâëåí âûâîä forms

--- otchet/forms.py
+++ otchet/forms.py
@@ -1,46 +1,44 @@
 # -*- coding: utf-8 -*-
 from .models import CustomUser, s_group, s_office, s_timetable, s_fault, s_drop_lift, s_commit
-from django import forms 
+from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from datetime import datetime
-# from worksite import settings
-import html5.forms.widgets as html5
 
 
 class AdminUserAddForm(UserCreationForm):
-
     class Meta:
         model = CustomUser
-        fields  = '__all__'
+        fields = '__all__'
         # verbose_name = "AdminUserAddForm"
         # verbose_name_plural = "AdminUserAddForms"
 
     def clean_username(self):
         username = self.cleaned_data['username']
         try:
-            CustomUser._default_manager.get(username=username)
+            CustomUser._default_manager.get(username = username)
         except CustomUser.DoesNotExist:
             return username
         raise forms.ValidationsError(self.error_messages['duplicate_username'])
 
-class AdminUserChangeForm(UserChangeForm):
 
+class AdminUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields  = '__all__'
+        fields = '__all__'
+
 
 class s_faultForm(forms.ModelForm):
     # error_css_class = 'large-4 medium-4 columns'
     # required_css_class = 'large-4 medium-4 columns'
     # htmlClass = 'large-4 medium-4 columns'
     fault_time = forms.DateTimeField(
-        label='Время', 
-        widget=forms.TextInput(attrs={      'type': 'datetime',
-                                            'value': datetime.now().strftime('%d.%m.%Y %H:%M'),
-                                            # 'class': 'large-4 medium-4 columns',
-                                            # 'data-date-time': ''
-                                            }))
-    description = forms.CharField(widget=forms.Textarea(attrs={'rows': '2'}), label='Описание:', )
+        label = 'Время',
+        widget = forms.DateTimeInput(attrs = {'type': 'datetime-local',
+                                          'value': datetime.now().strftime('%Y-%m-%dT%H:%M'),
+                                          # 'class': 'large-4 medium-4 columns',
+                                          # 'data-date-time': ''
+                                          }))
+    description = forms.CharField(widget = forms.Textarea(attrs = {'rows': '2'}), label = 'Описание:', )
 
     class Meta:
         model = s_fault
@@ -61,34 +59,45 @@ class s_faultForm(forms.ModelForm):
             errors_on_separate_row = False)
 
 
-
 class s_drop_liftForm(forms.ModelForm):
     # error_css_class = 'large-4 medium-4 columns'
     # required_css_class = 'large-4 medium-4 columns'
     stop_lift = forms.DateTimeField(
-        label = 'Время остановки', 
-        widget = forms.TextInput(attrs={
-            'type': 'datetime', 
+        label = 'Время остановки',
+        widget = forms.DateTimeInput(attrs = {
+            'type': 'datetime-local',
             # 'value': datetime.now().strftime('%d.%m.%Y %H:%M'),
-            }),
-        initial = datetime.now().strftime('%d.%m.%Y %H:%M'),
-        # input_formats = '%Y-%m-%dT%H:%M',
-        )
+        },
+            # format = '%Y-%m-%dT%H:%M'
+        ),
+        initial = datetime.now().strftime('%Y-%m-%dT%H:%M'),
+        input_formats = '%Y-%m-%dT%H:%M',
+    )
     start_lift = forms.DateTimeField(
         label = 'Время запуска',
         # widget = { 'thedate' : html5.DateTimeLocalInput }
-        widget=forms.TextInput(attrs={
-            'type': 'datetime', 
+        widget = forms.DateTimeInput(attrs = {
+            'type': 'datetime-local',
+            # 'format': '%Y-%m-%dT%H:%M',
             # 'value': datetime.now().strftime('%d.%m.%Y %H:%M'),
-            }),
-        initial = datetime.now().strftime('%d.%m.%Y %H:%M'),
-        # input_formats = '%Y-%m-%dT%H:%M',
+        },
+            # format = '%Y-%m-%dT%H:%M',
+        ),
+        initial = datetime.now().strftime('%Y-%m-%dT%H:%M'),
+        input_formats = '%Y-%m-%dT%H:%M',
         # input_formats = settings.DATE_INPUT_FORMATS,
-        )
-    description = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows': '2'}), label='Описание:', )
+    )
+    description = forms.CharField(required = False, widget = forms.Textarea(attrs = {'rows': '2'}),
+                                  label = 'Описание:', )
+
     class Meta:
-        model   = s_drop_lift
-        fields  = '__all__'
+        model = s_drop_lift
+        fields = '__all__'
+
+    # def is_valid(self):
+    #     self.start_lift = self.start_lift.strftime('')
+    #     obj = super(s_drop_liftForm, self).is_valid()
+
 
     def as_div(self):
         "Returns this form rendered as HTML <div>s."
@@ -101,19 +110,23 @@ class s_drop_liftForm(forms.ModelForm):
 
 
 class s_commitForm(forms.ModelForm):
-    s_prim = forms.CharField(widget=forms.Textarea(attrs={'rows': '2'}), label='Примечание:', )
+    s_prim = forms.CharField(widget = forms.Textarea(attrs = {'rows': '2'}), label = 'Примечание:', )
+
     class Meta:
         model = s_commit
         fields = ('s_prim',)
 
+
 class SMForm(forms.ModelForm):
     error_css_class = 'large-4 medium-4 columns'
     required_css_class = 'large-4 medium-4 columns'
+
     def __init__(self, *args, **kwargs):
-        super(ModelForm, self).__init__(*args, **kwargs)
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
         # adding css classes to widgets without define the fields:
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'test'
+
     def as_div(self):
         "Returns this form rendered as HTML <div>s."
         return self._html_output(
--- otchet/views.py
+++ otchet/views.py
@@ -7,12 +7,15 @@ from django.contrib import auth
 from django.core.context_processors import csrf
 from django.core.mail import send_mail
 # from django.contrib.flatpages.models import FlatPage
-from django.template import Context, loader 
+from django.template import Context, loader
 from datetime import datetime, timedelta, date
+
+
 # from django.utils import json
 
 @login_required
-def list_otchet(request, start_time=date.today()-timedelta(days=1), stop_time=date.today()+timedelta(days=1)):
+def list_otchet(request, start_time = date.today() - timedelta(days = 1),
+                stop_time = date.today() + timedelta(days = 1)):
     args = {}
     # args.update(csrf(request))
     # faults = s_fault.objects.select_related()
@@ -24,50 +27,54 @@ def list_otchet(request, start_time=date.today()-timedelta(days=1), stop_time=da
             stop_time = datetime.strptime(request.GET.get('stop_time'), '%Y-%m-%dT%H:%M')
     args['start_time'] = start_time.strftime('%Y-%m-%dT%H:%M')
     args['stop_time'] = stop_time.strftime('%Y-%m-%dT%H:%M')
-    args['faults'] = s_fault.objects.filter(fault_time__gte=start_time, fault_time__lte=stop_time)
-    args['lifts'] = s_drop_lift.objects.filter(stop_lift__gte=start_time, stop_lift__lte=stop_time)
+    args['faults'] = s_fault.objects.filter(fault_time__gte = start_time, fault_time__lte = stop_time)
+    args['lifts'] = s_drop_lift.objects.filter(stop_lift__gte = start_time, stop_lift__lte = stop_time)
     # faults = faults.filter(fault_time__lte=stop_time)
     # lifts = lifts.filter(stop_lift__lte=stop_time)
     # args['faults'] = faults
     # args['lifts'] = lifts
-    args['username'] = request.user.last_name+' '+request.user.first_name
+    args['username'] = request.user.last_name + ' ' + request.user.first_name
     return render_to_response('otchet.html', args)
+
 
 # TODO: Сделать нормально, без редиректа.
 @login_required
 def profile(request):
     return redirect('/')
 
+
 @login_required
 def index(request):
-    return render_to_response('index.html',)
+    return render_to_response('index.html', )
+
 
 def logout(request):
     auth.logout(request)
     return redirect('/accounts/login/')
 
+
 @login_required
-def newpost(request, postid=0, liftid=0):
-    com = s_commit.objects.order_by('-id')[0] # получаем последний закоммиченный объект
+def newpost(request, postid = 0, liftid = 0):
+    com = s_commit.objects.order_by('-id')[0]  # получаем последний закоммиченный объект
     postid = int(postid)
     args = {}
     args.update(csrf(request))
     # if request.GET:
     #     postid = int(request.GET['data'])
     if postid:
-        relative = get_object_or_404(s_fault, pk=postid)
+        relative = get_object_or_404(s_fault, pk = postid)
         if postid > com.s_fault_commit_id:
-        # b = s_fault.objects.filter(pk=postid).select_related()
-        # for a in b:
+            # b = s_fault.objects.filter(pk=postid).select_related()
+            # for a in b:
             # args['form'] = s_faultForm(initial=a)
-            args['form'] = s_faultForm(initial={
+            args['form'] = s_faultForm(initial = {
                 # 'fault_time': (relative.fault_time+timedelta(hours=5)).strftime('%d.%m.%Y %H:%M'),
-                'fault_time': (relative.fault_time+timedelta(hours=5)).strftime('%d.%m.%Y %H:%M'),
+                'fault_time': (relative.fault_time + timedelta(hours = 5)).strftime('%Y-%m-%dT%H:%M'),
                 'f_system': relative.f_system,
                 's_object': relative.s_object,
                 'description': relative.description,
                 'correction': relative.correction,
-                })
+            })
             args['buttonName'] = 'Записать'
             args['postid'] = postid
     else:
@@ -76,71 +83,75 @@ def newpost(request, postid=0, liftid=0):
         args['postid'] = 0
     liftid = int(liftid)
     if liftid:
-        liftbd = get_object_or_404(s_drop_lift, pk=liftid)
+        liftbd = get_object_or_404(s_drop_lift, pk = liftid)
         if liftid > com.s_lift_commit_id:
-            args['lift_form'] = s_drop_liftForm(initial={
-                'stop_lift' : (liftbd.stop_lift+timedelta(hours=5)).strftime('%d.%m.%Y %H:%M'),
-                'start_lift' : (liftbd.start_lift+timedelta(hours=5)).strftime('%d.%m.%Y %H:%M'),
+            args['lift_form'] = s_drop_liftForm(initial = {
+                'stop_lift': (liftbd.stop_lift + timedelta(hours = 5)).strftime('%Y-%m-%dT%H:%M'),
+                'start_lift': (liftbd.start_lift + timedelta(hours = 5)).strftime('%Y-%m-%dT%H:%M'),
                 'lift_name': liftbd.lift_name,
-                'fault' : liftbd.fault,
-                'description' : liftbd.description,
-                'consequences' : liftbd.consequences,
-                })
+                'fault': liftbd.fault,
+                'description': liftbd.description,
+                'consequences': liftbd.consequences,
+            })
             args['button_lift_name'] = 'Записать'
             args['liftid'] = liftid
     else:
         args['lift_form'] = s_drop_liftForm
         args['button_lift_name'] = 'Добавить'
         args['liftid'] = 0
-    args['faults'] = s_fault.objects.select_related().filter(pk__gt=com.s_fault_commit_id)
-    args['lifts'] = s_drop_lift.objects.select_related().filter(pk__gt=com.s_lift_commit_id)
+    args['faults'] = s_fault.objects.select_related().filter(pk__gt = com.s_fault_commit_id)
+    args['lifts'] = s_drop_lift.objects.select_related().filter(pk__gt = com.s_lift_commit_id)
     args['commit_form'] = s_commitForm
-    args['username'] = request.user.last_name+' '+request.user.first_name
+    args['username'] = request.user.last_name + ' ' + request.user.first_name
     return render_to_response('newpost.html', args)
+
 
 @login_required
 def addpost(request):
     if request.POST:
-        form = s_faultForm(request.POST)
-        if form.is_valid:
-            npost = form.save(commit=False)
-            npost.f_staff = CustomUser.objects.get(username=request.user.username)
+        cPost = request.POST.copy()
+        cPost['fault_time'] = datetime.strptime(cPost['fault_time'], '%Y-%m-%dT%H:%M')
+        form = s_faultForm(cPost)
+        if form.is_valid():
+            # try:
+            npost = form.save(commit = False)
+            npost.f_staff = CustomUser.objects.get(username = request.user.username)
             postid = int(request.POST.get('postid'))
             if postid:
-                npost.pk=postid
+                npost.pk = postid
             form.save()
     return redirect('/')
+    # return render_to_response('newpost.html', locals())
+
 
 @login_required
 def delpost(request, postid):
-    com = s_commit.objects.order_by('-id')[0] # получаем последний закоммиченный объект
+    com = s_commit.objects.order_by('-id')[0]  # получаем последний закоммиченный объект
     postid = int(postid)
     if postid > com.s_fault_commit_id:
         try:
-            s_fault.objects.get(pk=postid).delete()
+            s_fault.objects.get(pk = postid).delete()
         except:
             pass
 
     return redirect('/')
 
+
 @login_required
 def addliftfault(request):
     if request.POST:
-        # request.POST['start_lift'] = datetime.strptime(request.POST.get('start_lift'),'%Y-%m-%dT%H:%M')
         test = request.POST.copy()
-        # test['start_lift'] = '2007-07-07 07:07'
-        # test['start_lift'] = datetime.strptime(str(test['start_lift']), '%Y-%m-%dT%H:%M')
-        # test['start_lift'] = str(test['start_lift'])[:9]+' '+str(test['start_lift'])[11:]
-        # '2015-08-20T14:46'
+        test['stop_lift'] = datetime.strptime(test['stop_lift'], '%Y-%m-%dT%H:%M')
+        test['start_lift'] = datetime.strptime(test['start_lift'], '%Y-%m-%dT%H:%M')
         form = s_drop_liftForm(test)
-        # form['start_lift'] = test
-        if form.is_valid:
-            nlift = form.save(commit=False)
+        if form.is_valid():
+            nlift = form.save(commit = False)
             liftid = int(request.POST.get('liftid'))
             if liftid:
                 nlift.pk = liftid
             form.save()
     return redirect('/')
+
 
 @login_required
 def newmail(request):
@@ -148,30 +159,31 @@ def newmail(request):
     com = s_commit.objects.order_by('-id')[0]
     t = loader.get_template('sendotchet.html')
     args = {}
-    args['faults'] = s_fault.objects.select_related().filter(pk__gt=com.s_fault_commit_id)
-    args['lifts'] = s_drop_lift.objects.select_related().filter(pk__gt=com.s_lift_commit_id)
-    args['username'] = request.user.last_name+' '+request.user.first_name
+    args['faults'] = s_fault.objects.select_related().filter(pk__gt = com.s_fault_commit_id)
+    args['lifts'] = s_drop_lift.objects.select_related().filter(pk__gt = com.s_lift_commit_id)
+    args['username'] = request.user.last_name + ' ' + request.user.first_name
     args['d_otch'] = datetime.now()
     args['prim'] = ''
     if request.POST:
         form = s_commitForm(request.POST)
-        if form.is_valid and request.POST['s_prim']:
+        if form.is_valid() and request.POST['s_prim']:
             args['prim'] = request.POST['s_prim']
     body_html = t.render(Context(args))
     send_mail(
-        'Сдача смены '+ datetime.today(),
+        'Сдача смены ' + str(datetime.today()) + args['username'],
         args['username'],
         '5704@koltsovo.ru',
-        ['tich31337@gmail.com',],
-        html_message=body_html,)
+        ['tich31337@gmail.com', ],
+        html_message = body_html, )
     commit = s_commit(
-        s_time= datetime.now(),
+        s_time = datetime.now(),
         s_fault_commit = s_fault.objects.order_by('-id')[0],
         s_lift_commit = s_drop_lift.objects.order_by('-id')[0],
         s_prim = args['prim'],
-        s_staff_commit = CustomUser.objects.get(username=request.user.username),)
+        s_staff_commit = CustomUser.objects.get(username = request.user.username), )
     commit.save()
     return redirect('/')
+
 
 @login_required
 def fault_correct(request):
@@ -180,15 +192,16 @@ def fault_correct(request):
         com = s_commit.objects.order_by('-id')[0]
         postid = int(request.GET['data'])
         if postid > com.s_fault_commit_id:
-            postdb = s_fault.objects.get(pk=postid)
+            postdb = s_fault.objects.get(pk = postid)
             postdb.correction = not postdb.correction
             postdb.save()
     except:
         pass
         # context['text'] = 'error'
-    # else:
+        # else:
         # context['text'] = data[::-1]
     return redirect('/')
+
 
 @login_required
 def lift_del(request):
@@ -196,7 +209,7 @@ def lift_del(request):
     com = s_commit.objects.order_by('-id')[0]
     postid = int(request.GET['data'])
     if postid > com.s_lift_commit_id:
-        s_drop_lift.objects.get(pk=postid).delete()
-    # except:
+        s_drop_lift.objects.get(pk = postid).delete()
+        # except:
         # pass
-    return redirect('/')    
+    return redirect('/')
