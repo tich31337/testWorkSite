beedff9e3aca754a960c35f5d8c03d2ab1e35de3
tich <tich31337@ya.ru>
2016-02-05 01:16:10 +0500

    Добавлено новое приложение electro. Создана модель.

---
+++ electro/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import nameCount, energosfera
+# Register your models here.
+
+admin.site.register(energosfera)
+admin.site.register(nameCount)
---
+++ electro/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from otchet.models import s_objects
+# Create your models here.
+
+class energosfera(models.Model):
+
+    class Meta:
+        verbose_name        = 'Потребление'
+        verbose_name_plural = 'Потребление'
+        ordering = ['eDate',]
+        permissions = (('can_read_electro', 'Потребление'),)
+
+    def __str__(self):
+        return '%s %s %s' % self.ekwt, self.eId, self.eDate
+
+    ekwt = models.IntegerField(verbose_name = 'Потребление',)
+    eId = models.ForeignKey('nameCount',verbose_name = 'Счетчик')
+    eDate = models.DateTimeField(verbose_name = 'Время',)
+    
+class nameCount(models.Model):
+	
+    class Meta:
+        verbose_name        = 'Счетчик'
+        verbose_name_plural = 'Счетчик'
+        ordering = ['nBuild','nCount',]
+
+    def __str__(self):
+        return '{0} {1} {2}'.format(self.nBuild, self.nCount, self.nId,)
+
+    nCount = models.CharField('Название', max_length=50)
+    nId = models.IntegerField(verbose_name = 'Идентификатор', unique = True)
+    nBuild = models.ForeignKey('otchet.s_objects',verbose_name = 'Объект', blank = True, null = True)
